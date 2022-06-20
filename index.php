a327e03179b7c061d4c6b9fe6b54844eba0af29b
wujiehao <jiehao@busionline.com>
2022-06-20 09:51:07 +0800

    11

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require "vendor/autoload.php";
+
+$Test = new Wotu\Test();
+
+$Test->test();
---
+++ src/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace Wotu;
+class Test{
+    public function test(){
+        var_dump('composer-test');
+        exit;
+    }
+}
